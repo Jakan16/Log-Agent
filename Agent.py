70bde1b1bba3a7d576d2b315e8befe7052dfcb23
Jakan16 <porygom@gmail.com>
2019-12-11 21:10:44 +0100

    Now sends it with proper json header, and sends auth token correctly

--- Agent.py
+++ Agent.py
@@ -14,11 +14,11 @@ mypath = os.path.dirname(os.path.realpath(__file__))
 #load in all files in current path
 uploaded_files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f!='Agent.py' and f!='.DS_Store' ]
 
-#print to terminal which files is within the folder 
+#print to terminal which files is within the folder
 print("Files in current folder is " + str(uploaded_files))
 
-#Agent token - where does it come from?? 
-token = "42"
+#Agent token - where does it come from??
+token = "valid_agent_token"
 
 #function that composes token, file_content and timestamp
 def post_content(file_name, token):
@@ -26,20 +26,21 @@ def post_content(file_name, token):
     file_content = open_file.read()
     current_milli_time = int(round(time.time() * 1000))
     data = {}
-    data['agent'] = token
+    data['authorization'] = token
     data['timestamp'] = current_milli_time
     data['log'] = file_content
-    r = requests.post('http://localhost:8000', data=json.dumps(data, ensure_ascii=False).encode('utf-8'))
+    r = requests.post('http://localhost:7999/submitLog', json=data)
+    print( r.json() )
 
 for i in uploaded_files:
     post_content(i,token)
 
-agent = True 
+agent = True
 
-def Diff(li1, li2): 
-    return (list(set(li1) - set(li2))) 
+def Diff(li1, li2):
+    return (list(set(li1) - set(li2)))
 
-while agent == True: 
+while agent == True:
     current_files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and f!='Agent.py' and f!='.DS_Store']
     if uploaded_files == current_files:
         time.sleep(10)
@@ -50,6 +51,5 @@ while agent == True:
 
         for i in new_files:
             post_content(i,token)
-        
-        uploaded_files = current_files
 
+        uploaded_files = current_files
